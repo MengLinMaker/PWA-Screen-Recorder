f3ca8f4eb7e5ba81f3c735b1b5beab62285413ab
Meng Lin <lm8100mystery@gmail.com>
2023-02-14 01:19:24 +1100

    Fix bug - allow cancel and custom/default name

--- src/components/RecordBtn/RecordBtn.jsx
+++ src/components/RecordBtn/RecordBtn.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import './RecordBtn.scss'
+import "./RecordBtn.scss";
 
-function RecordBtn({source}) {
-  const [isRecording, setIsRecording] = useState(false)
+function RecordBtn({ source }) {
+  const [isRecording, setIsRecording] = useState(false);
   const [mediaRecorder, setmediaRecorder] = useState(null);
-  
-
 
   async function startRecording() {
     setIsRecording(true);
@@ -14,55 +12,64 @@ function RecordBtn({source}) {
       audioBitsPerSecond: 128000,
       videoBitsPerSecond: 8000000,
       mimeType: "video/webm; codecs=vp9,opus",
-    }
+    };
 
     let recordedChunks = [];
     const mediaRecorder = new MediaRecorder(source, options);
 
     mediaRecorder.ondataavailable = function (e) {
       if (e.data.size > 0) recordedChunks.push(e.data);
-    }
+    };
     mediaRecorder.onstop = function () {
-      saveFile(recordedChunks)
-      recordedChunks = []
-    }
-    mediaRecorder.start()
+      saveFile(recordedChunks);
+      recordedChunks = [];
+    };
+    mediaRecorder.start();
     setmediaRecorder(mediaRecorder);
   }
 
-
-
   async function stopRecording() {
-    setIsRecording(false)
+    setIsRecording(false);
     mediaRecorder.stop();
   }
 
-
-
   function saveFile(recordedChunks) {
     const blob = new Blob(recordedChunks, { type: "video/webm" });
-    let filename = window.prompt("Enter file name"),
-    
-    downloadLink = document.createElement("a");
+    let fileName = window.prompt("Enter file name"),
+      downloadLink = document.createElement("a");
     document.body.appendChild(downloadLink);
     downloadLink.href = URL.createObjectURL(blob);
-    downloadLink.download = `${filename}.mp4`;
-    downloadLink.click();
-    URL.revokeObjectURL(blob); // clear from memory
-    document.body.removeChild(downloadLink);
+
+    // Check if file is saved
+    if (fileName != null) {
+      // Give default name for unnamed video files
+      if (fileName == "") {
+        const dateTime = new Date()
+          .toLocaleString()
+          .replace(" ", "_")
+          .replace(",", "");
+        fileName = "Recording_" + dateTime;
+      }
+      downloadLink.download = `${fileName}`;
+      downloadLink.click();
+      URL.revokeObjectURL(blob); // clear from memory
+      document.body.removeChild(downloadLink);
+    }
   }
 
-
-
   return (
-    <div className='stickyBottom'>
-      {isRecording ? 
-      <button className="wideBtn stopBtn" onClick={stopRecording}>Stop</button>
-      :
-      <button className="wideBtn startBtn" onClick={startRecording}>Start</button>
-      }
+    <div className="stickyBottom">
+      {isRecording ? (
+        <button className="wideBtn stopBtn" onClick={stopRecording}>
+          Stop
+        </button>
+      ) : (
+        <button className="wideBtn startBtn" onClick={startRecording}>
+          Start
+        </button>
+      )}
     </div>
   );
 }
 
-export default RecordBtn
+export default RecordBtn;
