bc636434d93d3090fc52245af558cccb33288ec7
Meng Lin <lm8100mystery@gmail.com>
2023-02-16 13:11:16 +1100

    Remove semicolon format

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 
-import './App.scss'
-import { Navbar, RecordBtn, VideoPlayer, SourceInit } from './components'
+import "./App.scss"
+import { Navbar, RecordBtn, VideoPlayer, SourceInit } from "./components"
 
 function App() {
   const [source, setSource] = useState(false)
@@ -12,13 +12,13 @@ function App() {
       <RecordBtn source={source} />
     </>
   )
-  
+
   return (
     <div className="colflex">
       <Navbar />
-      {source ? elements : <SourceInit setSource={setSource}/>}
+      {source ? elements : <SourceInit setSource={setSource} />}
     </div>
-  );
+  )
 }
 
 export default App
