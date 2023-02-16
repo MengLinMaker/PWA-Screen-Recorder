7049e7c85488899a97137af7278a15645717550d
MengLinMaker <menglinmaker@gmail.com>
2023-02-16 23:09:23 +1100

    Add MengLinMaker logo link to Githib

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import './Navbar.scss'
+import React from "react"
+import "./Navbar.scss"
+import githubLogo from "../../assets/MengLinMaker.svg"
 
 function Navbar() {
-  
   return (
     <nav className="nav">
       <h3 className="nav--title">Web Screen Recorder</h3>
+      <a
+        className="github-link"
+        href="https://github.com/MengLinMaker/PWA-Screen-Recorder"
+        target="_blank"
+      >
+        <img src={githubLogo} className="icon" alt="Github logo" />
+        <h4>MengLinMaker</h4>
+      </a>
     </nav>
-  );
+  )
 }
 
 export default Navbar
